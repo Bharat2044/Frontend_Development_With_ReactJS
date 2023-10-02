95c6cf04e043710eb8fb8e5af7efca7e6e2883c4
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-10-02 03:45:56 +0000

    code added

---
+++ JS Programming Basics Refresher/06_operators-in-js/08_typeofOperators.js
@@ -0,0 +1,7 @@
+console.log(typeof 5);
+console.log(typeof 5n);
+console.log(typeof "5");
+console.log(typeof true);
+console.log(typeof(undefined));
+console.log(typeof(NaN));
+console.log(typeof(null));
