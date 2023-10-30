2ef4f2133540d1ab78490b4b52dd4983aaac6450
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-10-30 14:36:23 +0000

    code added

---
+++ 021_First Pillar of JavaScript - Coercion/03_ToNumberOnStrings.js
@@ -0,0 +1,28 @@
+console.log(1 + "450");
+console.log("5" + 3);
+console.log("5" + "3");
+console.log(1 + "12abc");
+console.log(0 + "0b1");
+console.log(1 + "0o11");
+console.log(1 + "0xA");
+
+console.log();
+
+console.log(1 - "450");
+console.log("5" - 3);
+console.log("5" - "3");
+console.log(1 - "12abc");
+console.log(0 - "0b1");
+console.log(1 - "0o11");
+console.log(1 - "0xA");
+
+console.log();
+
+console.log("10" - true);
+console.log("10" - false);
+console.log("10" - null);
+console.log("10" - undefined);
+console.log("10" - NaN);
+console.log("10" - 0xB);
+console.log("10" - 0b1);
+console.log("15" - 0o12);
