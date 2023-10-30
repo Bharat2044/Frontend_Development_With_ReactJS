c9a94b41470699f730e3753f4d7b2c25e5faaa59
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-10-30 06:32:01 +0000

    code added

---
+++ 016_JS Programming Basics Refresher [Optional]/06_operators-in-js/07_equalityOperators.js
@@ -0,0 +1,40 @@
+console.log(10 == 10);
+console.log(10 === 10);
+console.log(10 != 10);
+console.log(10 !== 10);
+
+console.log();
+
+console.log(1 == true);
+console.log(1 === true);
+
+console.log();
+
+console.log(5 == "5");
+console.log(5 === "5");
+console.log(5 != "5");
+console.log(5 !== "5");
+
+console.log();
+
+
+console.log(5 == "Raj");
+console.log(5 === "Raj");
+console.log(5 === NaN);
+console.log(5 === NaN);
+
+console.log();
+
+console.log(undefined == null);
+console.log(undefined === null);
+console.log(undefined == NaN);
+console.log(undefined === NaN);
+
+console.log();
+
+console.log(undefined == undefined);
+console.log(undefined === undefined);
+console.log(null == null);
+console.log(null === null);
+console.log(NaN == NaN);
+console.log(NaN === NaN);
