0f7b8be68f1e6939864c1e58e3d1e1f808c03d71
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-10-30 06:30:41 +0000

    code added

---
+++ 016_JS Programming Basics Refresher [Optional]/03_data-types-in-js/02_stringDatatype.js
@@ -0,0 +1,12 @@
+let a = "Raj";
+let b = 'Ramesh Kumar';
+let c = `Bharat Kumar`;
+let d = "100";
+let e = "1011 213298 raj";
+
+
+console.log("a = ", a);
+console.log("b = ", b);
+console.log("c = ", c);
+console.log("d = ", d);
+console.log("e = ", e);
