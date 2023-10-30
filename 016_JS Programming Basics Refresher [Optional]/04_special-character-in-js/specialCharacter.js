4695687cf1830eb028a81b97b58dcff647d36434
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-10-30 06:31:12 +0000

    code added

---
+++ 016_JS Programming Basics Refresher [Optional]/04_special-character-in-js/specialCharacter.js
@@ -0,0 +1,12 @@
+let a = "Hello, Raj \n How are you ?";
+
+let b = "Hello, Deepak \t Where are you ?";
+
+
+console.log("a = ", a);
+console.log("b = ", b);
+
+
+console.log("Hello Ji, \n Where are you from");
+console.log("Hello Ji, \t Where are you from");
+console.log("Hello Ji, \\ Where are you from");
