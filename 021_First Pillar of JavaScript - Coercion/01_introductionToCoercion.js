65d6d35017ff6428b66c41e1531436752baa913f
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-10-30 07:20:39 +0000

    code added

---
+++ 021_First Pillar of JavaScript - Coercion/01_introductionToCoercion.js
@@ -0,0 +1,35 @@
+console.log(5 + 3);
+console.log("5" + 3);
+console.log(5 + "3");
+console.log("5" + "3");
+console.log();
+
+console.log(5 - 3);
+console.log(5 - "3");
+console.log("5" - 3);
+console.log("5" - "3");
+console.log();
+
+console.log(5 * 3);
+console.log(5 * "3");
+console.log("5" * 3);
+console.log("5" * "3");
+console.log();
+
+console.log(5 / 3);
+console.log(5 / "3");
+console.log("5" / 3);
+console.log("5" / "3");
+console.log();
+
+console.log(5 % 3);
+console.log(5 % "3");
+console.log("5" % 3);
+console.log("5" % "3");
+console.log();
+
+console.log(5 ** 3);
+console.log(5 ** "3");
+console.log("5" ** 3);
+console.log("5" ** "3");
+console.log();
