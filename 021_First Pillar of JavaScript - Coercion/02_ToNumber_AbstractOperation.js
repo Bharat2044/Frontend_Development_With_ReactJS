ac2bbe8a86c9b1a899fc98a1ae302093cbb54403
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-10-30 08:57:03 +0000

    code added

---
+++ 021_First Pillar of JavaScript - Coercion/02_ToNumber_AbstractOperation.js
@@ -0,0 +1,24 @@
+console.log(10 + "10");
+console.log(10 - "10");
+console.log(10 * "10");
+console.log(10 / "10");
+console.log(10 % "10");
+console.log(10 ** "10");
+
+console.log();
+
+console.log(10 + "5");
+console.log(10 + true);
+console.log(10 + false);
+console.log(10 + null);
+console.log(10 + undefined);
+console.log(10 + NaN);
+
+console.log();
+
+console.log(10 - "5");
+console.log(10 - true);
+console.log(10 - false);
+console.log(10 - null);
+console.log(10 - undefined);
+console.log(10 - NaN);
