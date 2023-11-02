635a021f79db9d8cfdaf072347f1b7fcdc682f2d
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-11-02 23:23:36 +0000

    code added

---
+++ 021_First Pillar of JavaScript - Coercion/13_coercionWithUnaryPlusOperation.js
@@ -0,0 +1,7 @@
+console.log("10" + "  " + typeof("10"));
+console.log(+"10" + "  " + typeof(+"10"));
+
+console.log();
+
+console.log("Hi" + "  " + typeof("Hi"));
+console.log(+"Hi" + "  " + typeof(+"Hi"));
