1679b86ecfd5792cbc3871359af3da3edadfa156
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-11-04 14:18:27 +0000

    code added

---
+++ 021_First Pillar of JavaScript - Coercion/19_toBooleanInsideIfElse.js
@@ -0,0 +1,35 @@
+if(10) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+if(0) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+if(-123) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+if(undefined) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+if(NaN) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+if(null) {
+    console.log("true");
+} else {
+    console.log("false");
+}
