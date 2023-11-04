f27b26bbda53931f085979902b9070556e9dca10
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-11-04 14:29:05 +0000

    code added

---
+++ 021_First Pillar of JavaScript - Coercion/17_ToBoolean_AbstractOperation.js
@@ -0,0 +1,11 @@
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(0));
+console.log(Boolean(+0));
+console.log(Boolean(-0));
+console.log(Boolean(NaN));
+console.log(Boolean(""));
+console.log(Boolean("Joey Rules"));
+console.log(Boolean(100));
+console.log(Boolean({}));
+console.log(Boolean([]));
