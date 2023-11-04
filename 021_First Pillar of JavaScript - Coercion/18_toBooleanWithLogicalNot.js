1db0caaa4222714dbbb2709af1ca4c6a20af4a2d
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-11-04 14:18:22 +0000

    code added

---
+++ 021_First Pillar of JavaScript - Coercion/18_toBooleanWithLogicalNot.js
@@ -0,0 +1,14 @@
+console.log(!10);
+console.log(!0);
+console.log(!(+0));
+console.log(!(-0));
+console.log(!NaN);
+console.log(!undefined);
+console.log(!null);
+console.log(!true);
+console.log(!false);
+console.log(!"");
+console.log(!" ");
+console.log(!"Hi");
+console.log(!{});
+console.log(!{x: 10});
