13b7578f0734899ef93b1ce9e5fcbbc31187821f
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-11-05 17:28:26 +0000

    code added

--- 021_First Pillar of JavaScript - Coercion/17_ToBoolean_AbstractOperation.js
+++ 021_First Pillar of JavaScript - Coercion/17_ToBoolean_AbstractOperation.js
@@ -1,11 +1,25 @@
-console.log(Boolean(null));
-console.log(Boolean(undefined));
-console.log(Boolean(0));
-console.log(Boolean(+0));
-console.log(Boolean(-0));
-console.log(Boolean(NaN));
-console.log(Boolean(""));
-console.log(Boolean("Joey Rules"));
-console.log(Boolean(100));
-console.log(Boolean({}));
-console.log(Boolean([]));
+console.log(true + null);
+console.log(true + undefined);
+console.log(true + 0);
+console.log(true + (+0));
+console.log(true + (-0));
+console.log(true + NaN);
+console.log(true + "");
+console.log(true + "Joey Rules");
+console.log(true + 100);
+console.log(true + {});
+console.log(true + []);
+
+console.log();
+
+console.log(false + null);
+console.log(false + undefined);
+console.log(false + 0);
+console.log(false + (+0));
+console.log(false + (-0));
+console.log(false + NaN);
+console.log(false + "");
+console.log(false + "Joey Rules");
+console.log(false + 100);
+console.log(false + {});
+console.log(false + []);
