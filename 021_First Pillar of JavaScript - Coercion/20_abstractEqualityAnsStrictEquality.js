2fc4d2ee1f6ea0f9d6c46d28292b4c0634e3a19e
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-11-05 04:45:26 +0000

    code added

---
+++ 021_First Pillar of JavaScript - Coercion/20_abstractEqualityAnsStrictEquality.js
@@ -0,0 +1,22 @@
+console.log(10 == 10);
+console.log(10 === 10);
+
+console.log();
+
+console.log("10" == 10);
+console.log("10" === 10);
+
+console.log();
+
+console.log(null == undefined);
+console.log(null === undefined);
+
+console.log();
+
+console.log(null == NaN);
+console.log(null === NaN);
+
+console.log();
+
+console.log(undefined == NaN);
+console.log(undefined === NaN);
