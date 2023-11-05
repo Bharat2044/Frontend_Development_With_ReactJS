7e0bfd57f14919979993ff012bd83e87ba1f08cc
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-11-05 18:04:10 +0000

    code added

---
+++ 021_First Pillar of JavaScript - Coercion/30_boxing.js
@@ -0,0 +1,6 @@
+// console.log(1.toString());   // TypeError: Cannot read properties of undefined (reading 'toString')
+// console.log(undefined.toString());   // TypeError: Cannot read properties of undefined (reading 'toString')
+
+console.log('abc'.toString());
+console.log((1).toString());
+console.log(Number(1).toString());
