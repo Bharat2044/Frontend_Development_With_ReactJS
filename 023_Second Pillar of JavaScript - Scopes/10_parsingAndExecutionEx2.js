0708b700603d72228ac6e78578c0287fdfa5cd51
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-11-20 16:05:39 +0000

    code added

---
+++ 023_Second Pillar of JavaScript - Scopes/10_parsingAndExecutionEx2.js
@@ -0,0 +1,18 @@
+let teacher = "Sanket";
+
+function fun() {
+    var teacher = "Pulkit";
+    console.log(teacher);
+}
+
+function gun() {
+    var student = "Bharat";
+    content = "JavaScript";
+    console.log(student);
+}
+
+fun();
+gun();
+
+console.log(teacher);
+console.log(content);
