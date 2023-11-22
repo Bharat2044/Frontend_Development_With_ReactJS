e644323e53a8e0e79e7fb4c8d4554d194ae5631a
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-11-22 21:50:18 +0000

    code added

---
+++ 029_Lets make the DOM move with JavaScript/05_getElementById and getElementByClassName/index.js
@@ -0,0 +1,8 @@
+document.getElementById("top-main-text").style.fontSize = "50px";
+document.getElementById("top-main-text").style.color = "yellow";
+
+console.log(document.getElementsByClassName("image"));
+console.log(document.getElementsByTagName("section"));
+console.log(document.getElementsByTagName("p"));
+
+document.getElementsByTagName("p")[0].style.color = "purple"
