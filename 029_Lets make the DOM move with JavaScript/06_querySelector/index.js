d26a03b59bed9abe6857007648e62857c39b90a4
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-11-23 05:33:01 +0000

    code added

---
+++ 029_Lets make the DOM move with JavaScript/06_querySelector/index.js
@@ -0,0 +1,12 @@
+document.querySelector("#top-main-text").style.fontSize = "50px";
+document.querySelector("#top-main-text").style.color = "yellow";
+
+console.log(document.querySelector(".image"));
+console.log(document.querySelectorAll(".image"));
+
+console.log(document.querySelector("section"));
+console.log(document.querySelectorAll("p"));
+
+document.querySelectorAll("p")[0].style.color = "purple"
+
+console.log(document.querySelectorAll(".image div"));
