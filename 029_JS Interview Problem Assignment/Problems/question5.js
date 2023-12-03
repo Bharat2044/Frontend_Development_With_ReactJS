b6cf18f127e9a62ab7f7428cd2c7471c8233db4c
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-03 01:12:31 +0000

    Added problems

---
+++ 029_JS Interview Problem Assignment/Problems/question5.js
@@ -0,0 +1,24 @@
+/**
+ * 
+Implement a function deepEqual that performs a deep comparison between two 
+values. It returns true if two input values are deemed equal, and returns 
+false if not.
+
+You can assume there are only JSON-serializable values (numbers, strings, 
+boolean, null, objects, arrays).
+There wouldn't be cyclic objects, i.e. objects with circular references.
+Examples
+    deepEqual('foo', 'foo'); // true
+    deepEqual({ id: 1 }, { id: 1 }); // true
+    deepEqual([1, 2, 3], [1, 2, 3]); // true
+    deepEqual([{ id: '1' }], [{ id: '2' }]); // false
+ */
+
+/**
+ * @param {*} valueA
+ * @param {*} valueB
+ * @return {boolean}
+ */
+export default function deepEqual(valueA, valueB) {
+    throw 'Not implemented!';
+  }
