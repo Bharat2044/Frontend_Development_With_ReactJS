ffe3a5d5d8000afddcfe7fb314d3b83e15febe20
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-03 01:12:50 +0000

    Added problems

---
+++ 029_JS Interview Problem Assignment/Tests/question4.test.js
@@ -0,0 +1,21 @@
+import chunk from '../Problems/question4';
+
+describe('chunk', () => {
+  test('empty array', () => {
+    expect(chunk([], 3)).toEqual([]);
+  });
+
+  test('single-element array', () => {
+    expect(chunk([1], 3)).toEqual([[1]]);
+  });
+
+  test('splits into chunks', () => {
+    expect(chunk([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 3)).toEqual([
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [10, 11],
+    ]);
+  });
+  
+});
