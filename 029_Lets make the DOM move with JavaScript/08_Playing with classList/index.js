99da670d5daab39b17c3e33c1ce125a9c4111cf2
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-03 00:37:13 +0000

    code added

--- 029_Lets make the DOM move with JavaScript/08_Playing with classList/index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("top-main-text").classList.add("custom");
-
-// document.getElementById("top-main-text").classList.remove("custom");
-
-// document.getElementById("top-main-text").classList.toggle("custom");
-
-console.log(document.getElementById("top-main-text").classList.contains("custom"));
-
- 
