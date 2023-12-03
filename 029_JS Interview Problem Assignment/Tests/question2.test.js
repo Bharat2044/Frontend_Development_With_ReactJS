08bd7c4ff6d079b897bffeedbd3321607ceeea85
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-03 01:12:41 +0000

    Added problems

---
+++ 029_JS Interview Problem Assignment/Tests/question2.test.js
@@ -0,0 +1,20 @@
+import makeCounter from "../Problems/question2";
+describe('makeCounter', () => { 
+
+    test('returns a function', () => {
+        const counter = makeCounter();
+        expect(counter).toBeInstanceOf(Function);
+    });
+
+    test('return default value', () => {
+        const counter = makeCounter();
+        expect(counter()).toBe(0);
+    });
+
+    test('increments', () => {
+        const counter = makeCounter();
+        expect(counter()).toBe(0);
+        expect(counter()).toBe(1);
+        expect(counter()).toBe(2);
+    });
+})
