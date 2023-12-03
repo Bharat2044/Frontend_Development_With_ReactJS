2ca20eee2568edcdfcf02e0549b4a36bec933ecd
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-03 01:12:44 +0000

    Added problems

---
+++ 029_JS Interview Problem Assignment/Tests/question5.test.js
@@ -0,0 +1,23 @@
+import deepEqual from '../Problems/question5';
+
+describe('deepEqual', () => {
+  test('primitive values', () => {
+    expect(deepEqual(0, 0)).toEqual(true);
+    expect(deepEqual(true, 1)).toEqual(false);
+    expect(deepEqual(false, false)).toEqual(true);
+    expect(deepEqual(null, null)).toEqual(true);
+  });
+  
+  test('arrays', () => {
+    expect(deepEqual(['1'], ['1'])).toEqual(true);
+    expect(deepEqual([1, 2, 3], [1, 3, 2])).toEqual(false);
+  });
+
+  test('objects', () => {
+    expect(deepEqual({ foo: 'bar' }, { foo: 'bar' })).toEqual(true);
+    expect(deepEqual({ foo: 'bar', id: 1 }, { foo: 'bar', id: 1 })).toEqual(
+      true,
+    );
+  });
+
+});
