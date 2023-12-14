71eaa8fd79e769c4c99808bcef2957062988e419
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-14 02:25:32 +0000

    code added

---
+++ 038_Advanced DOM Events - Bubbling, Capture, Delegation/02_Event Bubbling/02_Event Bubbling Example1/index.js
@@ -0,0 +1,11 @@
+let d = document.getElementById("div");
+
+d.addEventListener("click", () => {
+    console.log("Clicked");
+})
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+    console.log("Clicked Button");
+})
