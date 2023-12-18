0ef6233a118bd0c905c1601060e7a7376772b589
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-18 01:02:55 +0000

    code added

---
+++ 040_Understanding NodeJS/03_Modue Pattern in NodeJS/02_ES6 Module/sorting.mjs
@@ -0,0 +1,145 @@
+export function insertionSort(arr) 
+{ 
+    let n = arr.length;
+	let i, key, j; 
+	for (i = 1; i < n; i++) 
+	{ 
+		key = arr[i]; 
+		j = i - 1; 
+
+		/* Move elements of arr[0..i-1], that are 
+		greater than key, to one position ahead 
+		of their current position */
+		while (j >= 0 && arr[j] > key) 
+		{ 
+			arr[j + 1] = arr[j]; 
+			j = j - 1; 
+		} 
+		arr[j + 1] = key; 
+	} 
+} 
+
+
+function swap(arr,xp, yp)
+{
+    var temp = arr[xp];
+    arr[xp] = arr[yp];
+    arr[yp] = temp;
+}
+ 
+export function selectionSort(arr)
+{
+    let n = arr.length;
+    var i, j, min_idx;
+ 
+    // One by one move boundary of unsorted subarray
+    for (i = 0; i < n-1; i++)
+    {
+        // Find the minimum element in unsorted array
+        min_idx = i;
+        for (j = i + 1; j < n; j++)
+        if (arr[j] < arr[min_idx])
+            min_idx = j;
+ 
+        // Swap the found minimum element with the first element
+        swap(arr,min_idx, i);
+    }
+}
+
+
+export function bubbleSort(arr)
+{
+    let n = arr.length;
+    var i, j, temp;
+    var swapped;
+    for (i = 0; i < n - 1; i++) 
+    {
+        swapped = false;
+        for (j = 0; j < n - i - 1; j++) 
+        {
+            if (arr[j] > arr[j + 1]) 
+            {
+                // Swap arr[j] and arr[j+1]
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+ 
+        // IF no two elements were 
+        // swapped by inner loop, then break
+        if (swapped == false)
+        break;
+    }
+}
+
+
+function merge(arr, l, m, r)
+{
+    var n1 = m - l + 1;
+    var n2 = r - m;
+ 
+    // Create temp arrays
+    var L = new Array(n1); 
+    var R = new Array(n2);
+ 
+    // Copy data to temp arrays L[] and R[]
+    for (var i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (var j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+ 
+    // Merge the temp arrays back into arr[l..r]
+ 
+    // Initial index of first subarray
+    var i = 0;
+ 
+    // Initial index of second subarray
+    var j = 0;
+ 
+    // Initial index of merged subarray
+    var k = l;
+ 
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    // Copy the remaining elements of
+    // L[], if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    // Copy the remaining elements of
+    // R[], if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+ 
+function f(arr,l, r){
+    if(l>=r){
+        return;
+    }
+    var m =l+ parseInt((r-l)/2);
+    f(arr,l,m);
+    f(arr,m+1,r);
+    merge(arr,l,m,r);
+}
+
+export default function mergeSort(arr) {
+    return f(arr, 0, arr.length-1)
+}
