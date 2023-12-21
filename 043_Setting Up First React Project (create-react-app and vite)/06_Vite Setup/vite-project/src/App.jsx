aa62749a83ef04cf306945dbe1f19e1cd5579f65
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-21 07:31:50 +0000

    code added

---
+++ 043_Setting Up First React Project (create-react-app and vite)/06_Vite Setup/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="App"> 
+        <button id="btn">
+          <span>Submit</span>
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
