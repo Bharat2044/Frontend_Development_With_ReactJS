d800a622ba98bd8cc6fed5e52399432b1412b600
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-21 03:50:01 +0000

    code added

---
+++ 041_Welcome to the ReactJS Universe/06_Glimpse Of JSX/App.js
@@ -0,0 +1,36 @@
+// import Raact from 'react';
+// import RaactDOM from 'react-dom/cliend';
+
+const Custom = () => {
+    return (
+        <nav>
+            <div>1</div>
+            <input></input>
+            <div>2</div>
+        </nav>        
+    );
+}
+
+const App = () => {
+    // return <div> <h1> Welcome to React </h1> <button> Click </button> </div>;
+    return (
+        <div>
+            <h1> 
+                Welcome to React 
+            </h1> 
+            <button>
+                Click
+            </button>
+
+            <Custom />
+            <Custom />
+            <Custom />
+            <Custom />
+        </div>
+    );
+}
+
+const rootNode = document.getElementById("root");
+const root = ReactDOM.createRoot(rootNode);
+
+root.render(<App />);
