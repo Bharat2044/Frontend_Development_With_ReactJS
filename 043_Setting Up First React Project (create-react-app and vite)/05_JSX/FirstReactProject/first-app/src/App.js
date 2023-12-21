ccbd9825e8b02592ad730b117207e3199fef1998
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-21 07:06:42 +0000

    code added

---
+++ 043_Setting Up First React Project (create-react-app and vite)/05_JSX/FirstReactProject/first-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  //return React.createElement("div", {}, React.createElement("h1", {}, "Welcome to React"));
+  
+  // JSX Syntax
+  return (
+    <div>
+      <div>Hi</div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
