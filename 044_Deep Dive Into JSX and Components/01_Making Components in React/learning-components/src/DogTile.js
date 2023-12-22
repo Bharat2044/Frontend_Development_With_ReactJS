14caa06ff627ce584a7b659158cb386cfcd8b711
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-22 12:01:47 +0000

    code added

--- 044_Deep Dive Into JSX and Components/01_Making Components in React/learning-components/src/DogTile.js
+++ 044_Deep Dive Into JSX and Components/01_Making Components in React/learning-components/src/DogTile.js
@@ -17,7 +17,7 @@ function DogTile() {
 function DogTile() {  
   // return <div> New Dog </div>;
   return (
-    <div>
+    <div className="first">
       <img src="https://img.freepik.com/free-photo/puppy-that-is-walking-snow_1340-37228.jpg" alt="puppy image"></img>
       <h3>Cute Puppy</h3>
     </div>
