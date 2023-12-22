1358e98e8fe711fce23c89d92a83d2ce20cd6225
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-22 14:07:54 +0000

    code added

---
+++ 044_Deep Dive Into JSX and Components/03_Fragment/learning-components/src/DogDescription.js
@@ -0,0 +1,24 @@
+import { Fragment } from "react";
+
+function DogDescription() {
+    return (
+        <Fragment>
+            This is a preety cute dog, isn't it ? :)
+        </Fragment>
+    );
+}
+
+export default DogDescription;
+
+
+/*
+function DogDescription() {
+    return (
+        <>
+            This is a preety cute dog, isn't it ? :)
+        </>
+    );
+}
+
+export default DogDescription;
+*/
