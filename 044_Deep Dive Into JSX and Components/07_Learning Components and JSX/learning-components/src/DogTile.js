8646e6a416f971beb93f65b36a3afad42be4d343
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-22 16:39:13 +0000

    Added todos with state

---
+++ 044_Deep Dive Into JSX and Components/07_Learning Components and JSX/learning-components/src/DogTile.js
@@ -0,0 +1,16 @@
+import './DogTile.css';
+import DogDescription from './DogDescription';
+
+function DogTile(props) {
+    console.log(props.children);
+    return (
+      <div className="first"> 
+        <img src={props.image} />
+        <h3 style={{fontSize: '20px', color: 'red'}}> {props.text} </h3>
+        <DogDescription description={props.description} />
+        {props.children}
+      </div>
+    );
+}
+
+export default DogTile;
