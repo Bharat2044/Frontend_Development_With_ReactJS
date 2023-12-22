317012f9fdf9f37e71919cfd3ec71d7d3cfa898e
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-22 14:40:35 +0000

    code added

--- 044_Deep Dive Into JSX and Components/03_Fragment/learning-components/src/DogTile.js
+++ 044_Deep Dive Into JSX and Components/03_Fragment/learning-components/src/DogTile.js
@@ -7,6 +7,7 @@ function DogTile() {
   return (
     <div className="first">
       <img src="https://img.freepik.com/free-photo/puppy-that-is-walking-snow_1340-37228.jpg" alt="puppy image"></img>
+      <h3>Cute Puppy</h3>
       <DogDescription />
     </div>
   );
