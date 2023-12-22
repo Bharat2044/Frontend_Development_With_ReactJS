820248ff37a509ce536adb4eb72efae86e2dc627
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-22 12:25:46 +0000

    code added

---
+++ 044_Deep Dive Into JSX and Components/02_className Prop/learning-components/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import DogTile from './DogTile';
+
+function App() {
+  return (
+    <div className='puppy'>
+      <DogTile />
+      <DogTile />
+      <DogTile />
+      <DogTile />
+    </div>
+  );
+}
+
+export default App;
