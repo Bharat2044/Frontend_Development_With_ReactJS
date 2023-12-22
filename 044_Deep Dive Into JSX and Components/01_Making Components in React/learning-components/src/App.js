531c6f81c10ec393c8b8f12468d5fe06554428ab
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-22 07:06:24 +0000

    code added

---
+++ 044_Deep Dive Into JSX and Components/01_Making Components in React/learning-components/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import DogTile from './DogTile';
+
+function App() {
+  return (
+    <div className='first'>
+      Welcome to learning ReactJs Components
+
+      <DogTile />
+      <DogTile />
+    </div>
+  );
+}
+
+export default App;
