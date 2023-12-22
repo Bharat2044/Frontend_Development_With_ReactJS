11ca2fd9e27c350c53707793f004035da0c46849
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-22 14:07:47 +0000

    code added

---
+++ 044_Deep Dive Into JSX and Components/03_Fragment/learning-components/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import DogTile from './DogTile';
+
+function App() {
+  return (
+    <section className='puppy'>
+      <DogTile />
+      <DogTile />
+      <DogTile />
+      <DogTile />
+    </section>
+  );
+}
+
+export default App;
