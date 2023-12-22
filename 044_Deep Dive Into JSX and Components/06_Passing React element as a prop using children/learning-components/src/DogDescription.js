2dc48855b41e861ea7b4738cfdaf1171eccfa7a5
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-12-22 16:31:03 +0000

    code added

---
+++ 044_Deep Dive Into JSX and Components/06_Passing React element as a prop using children/learning-components/src/DogDescription.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+function DogDescription({description}) {
+    return (
+        <Fragment>
+            {description}
+        </Fragment>
+    );
+}
+
+export default DogDescription;
+
