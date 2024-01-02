2c0701b87e0bd7af446708958161531aa91dc668
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-01-02 12:02:56 +0000

    code added

--- 046_Rendering Lists in React and Coding Todo App | React Profiler/06_Simple Use Case of Passing Data From Child to Parent/learning-components/src/Todo.js
+++ 046_Rendering Lists in React and Coding Todo App | React Profiler/06_Simple Use Case of Passing Data From Child to Parent/learning-components/src/Todo.js
@@ -1,10 +1,16 @@
 import TodoItem from "./TodoItem";
+import { useState } from "react";
 
 function Todo(props) {
-    const todoItems = props.todoList.map((todo) => <TodoItem todo={todo} />);
+    // const todoItems = props.todoList.map((todo) => <TodoItem todo={todo} />);
+    const [todoItems, setTodoItems] = useState([]);
 
     return (
-        <ul> {todoItems} </ul>
+        <>
+            {/* <ul> {todoItems} </ul> */}
+            <ul> {todoItems.map((todo) => <TodoItem todo={todo} key={todo.id} />)}</ul>
+        </>
+        
     );
 }
 
