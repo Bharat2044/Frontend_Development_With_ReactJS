c457c660c7822c4472748960da490f5974d87c46
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-01-02 16:42:14 +0000

    added basic todo

---
+++ 046_Rendering Lists in React and Coding Todo App | React Profiler/09_Todolist/react-theory/src/TodoItem.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "./TodoItem.css";
+
+
+function TodoItem(props) { 
+    const [isEditting, setIsEditting] = useState(false);
+    const [todoData, setTodoData] = useState(props.todo.data);
+
+    function editTodo(todoId, newTodo) {
+
+    }
+
+    return (
+        <div>
+            <li>
+                {
+                    (isEditting) ? 
+                    <input type="text" value={todoData} onChange={(e) => setTodoData(e.target.value)} /> : 
+                    <span>{props.todo.data}</span>
+                }
+            </li>
+
+            <button onClick={() => {
+                props.delete();
+            }}>Done</button>
+
+            <button onClick={() => {
+                setIsEditting(!isEditting);
+                props.edit(todoData);
+            }}> {(isEditting) ? "Save" : "Edit"}</button>
+        </div>
+    );
+}
+
+export default TodoItem
