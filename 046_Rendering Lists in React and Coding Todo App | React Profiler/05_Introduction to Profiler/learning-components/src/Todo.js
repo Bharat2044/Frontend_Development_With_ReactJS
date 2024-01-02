dc18d52bc15bbdfb50b7b2871385cfee87842eb7
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-01-02 11:05:20 +0000

    code added

--- 046_Rendering Lists in React and Coding Todo App | React Profiler/05_Introduction to Profiler/learning-components/src/Todo.js
+++ 046_Rendering Lists in React and Coding Todo App | React Profiler/05_Introduction to Profiler/learning-components/src/Todo.js
@@ -1,7 +1,7 @@
 import TodoItem from "./TodoItem";
 
 function Todo(props) {
-    const todoItems = props.todoList.map((todo, index) => <TodoItem key={todo.id} todo={todo} />);
+    const todoItems = props.todoList.map((todo) => <TodoItem todo={todo} />);
 
     return (
         <ul> {todoItems} </ul>
