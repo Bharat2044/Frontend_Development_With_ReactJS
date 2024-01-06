1f377af298cfeacfafd00bea83ae91223755b21d
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-01-06 16:09:13 +0000

    code added

---
+++ 047_Project #11 - Tic Tac Toe 🧩/tic-tac-toe/components/Icon/Icon.jsx
@@ -0,0 +1,15 @@
+import { FaTimes, FaPen, FaRegCircle } from "react-icons/fa"
+
+function Icon({ name }) {
+    if(name == "circle") {
+        return <FaRegCircle />;
+    }
+    else if(name == "cross") {
+        return <FaTimes />;
+    }
+    else {
+        return <FaPen />;
+    }
+}
+
+export default Icon;
