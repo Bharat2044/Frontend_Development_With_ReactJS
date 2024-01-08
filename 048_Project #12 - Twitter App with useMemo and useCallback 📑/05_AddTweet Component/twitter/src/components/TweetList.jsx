74e65db8c25213f1966994891ba2af90aa2219b5
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-01-08 14:44:25 +0000

    code added

---
+++ 048_Project #12 - Twitter App with useMemo and useCallback 📑/05_AddTweet Component/twitter/src/components/TweetList.jsx
@@ -0,0 +1,16 @@
+import Tweet from "./Tweet";
+import "../CSS/TweetList.css";
+
+function TweetList({ tweets }) {
+    return (
+        <ul className="tweet-list">
+            {tweets.map((tweet) => (
+                <li className="tweet-like-wrapper">
+                    <Tweet content={tweet.content} likeCount={tweet.likeCount} />
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export default TweetList;
